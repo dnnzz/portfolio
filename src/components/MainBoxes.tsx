bd1e2cddc50d9d1b76784fed21d0137fa5ebd1a0
Deniz Fırat <deniz.firat@gtsfly.de>
2023-08-29 15:44:09 +0300

    lint fix

--- src/components/MainBoxes.tsx
+++ src/components/MainBoxes.tsx
@@ -10,8 +10,8 @@ export const MainBoxes = () => {
          <BoxesContainer />
          <h1 className="md:text-4xl text-xl text-white relative z-20 !p-0 !m-0">Hello I&apos;m Deniz</h1>
          <p className="text-center mt-2 text-neutral-300 relative z-20">
-            "Experienced computer engineer specializing in front-end development. Crafting engaging user interfaces with modern tools. Check out my
-            portfolio!"
+            &quot;Experienced computer engineer specializing in front-end development. Crafting engaging user interfaces with modern tools. Check out
+            my portfolio!&quot;
          </p>
       </div>
    );
